d62da2765cc138087cd5e1d8444250236c71ffe6
Mayur Dayal <mayur.dayal97@gmail.com>
2022-05-02 15:29:12 +0530

    fetched api and displayed the meme data

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,21 +1,74 @@
 import React from "react";
+import API_CALL from "../API_CALL.json";
 
 const Meme = function () {
+  const [memeFound, setMemeFound] = React.useState([]);
+  const [randomMeme, setRandomMeme] = React.useState({
+    topContent: "",
+    btmContent: "",
+    memeUsed: "",
+  });
+
+  React.useEffect(() => {
+    fetch(API_CALL.API_URL)
+      .then((response) => response.json())
+      .then((memeData) => setMemeFound(memeData.data.memes));
+  }, []);
+
+  const handleNewMeme = function (event) {
+    event.preventDefault();
+    const randomMeme = Math.floor(Math.random() * memeFound.length);
+
+    setRandomMeme((prev) => {
+      return {
+        ...prev,
+        memeUsed: memeFound[randomMeme],
+      };
+    });
+  };
+
+  const handleContent = function (event) {
+    const { name, value } = event.target;
+
+    setRandomMeme((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
   return (
     <main>
       <form>
-        <input type="text" placeholder="Top Content" />
+        <input
+          type="text"
+          placeholder="Top Content"
+          name="topContent"
+          onChange={handleContent}
+          value={randomMeme.topContent}
+        />
 
-        <input type="text" placeholder="Bottom Content" />
+        <input
+          type="text"
+          placeholder="Bottom Content"
+          name="btmContent"
+          onChange={handleContent}
+          value={randomMeme.btmContent}
+        />
 
-        <button>Get a new meme image</button>
+        <button onClick={handleNewMeme}>Get a new meme image</button>
       </form>
 
       <div className="meme-display">
-        <img src="" alt="" className="meme-img" />
+        <img
+          src={randomMeme.memeUsed.url}
+          alt={randomMeme.memeUsed.name}
+          className="meme-img"
+        />
 
-        <h2 className="top-content-text"></h2>
-        <h2 className="btm-content-text"></h2>
+        <h2 className="top-content-text">{randomMeme.topContent}</h2>
+        <h2 className="btm-content-text">{randomMeme.btmContent}</h2>
       </div>
     </main>
   );
